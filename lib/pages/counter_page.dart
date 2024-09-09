5cf1a9cc1f64358763faa823c3fff575708b378e
Adeniyi odunuga <odunugaadeniyi@gmail.com>
2024-09-09 16:30:14 +0100

    App Update

--- lib/pages/counter_page.dart
+++ lib/pages/counter_page.dart
@@ -8,10 +8,8 @@ class CounterPage extends StatefulWidget {
 }
 
 class CounterPageState extends State<CounterPage> {
-
   //variable
-    int _counter = 0;
-
+  int _counter = 0;
 
   //method
   void _incrementCounter() {
@@ -24,33 +22,41 @@ class CounterPageState extends State<CounterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.lightBlue,
-      appBar:AppBar(
-        backgroundColor: const Color.fromARGB(255, 2, 49, 87),
-        elevation: 0,
-        title: const Text("BEV COUNTER_APP", style: TextStyle(color: Colors.deepOrange),)
-      ),
-      body: Center(
-        child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-        const Text("You push the button this many times", style: TextStyle(color:Colors.blue),),
-        
-
-            // Counter Value
-            Text(
-              _counter.toString(),
-              style: const TextStyle(fontSize: 35),
-            ),
-
-            //Button
-            ElevatedButton(
-              onPressed: _incrementCounter, 
-              child: const Text("increment!", style: TextStyle(color:Colors.red, fontSize:20,), )
+        backgroundColor: Colors.lightBlue,
+        appBar: AppBar(
+            backgroundColor: const Color.fromARGB(255, 2, 49, 87),
+            elevation: 0,
+            title: const Text(
+              "BEV_COUNTER_APP",
+              style: TextStyle(color: Colors.deepOrange),
+            )),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "You push the button this many times",
+                style: TextStyle(color: Colors.blue),
               ),
-          ],
-        ),
-      )
-    );
+
+              // Counter Value
+              Text(
+                _counter.toString(),
+                style: const TextStyle(fontSize: 35),
+              ),
+
+              //Button
+              ElevatedButton(
+                  onPressed: _incrementCounter,
+                  child: const Text(
+                    "increment!",
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 20,
+                    ),
+                  )),
+            ],
+          ),
+        ));
   }
 }
